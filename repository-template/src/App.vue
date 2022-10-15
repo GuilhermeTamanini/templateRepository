753ce701b52b1fff448cbaf25d5b31e9c48c69fb
Guilherme Tamanini <guilherme.gui.tama@gmail.com>
2022-10-15 19:58:58 -0300

    InitialTemplate

---
+++ repository-template/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Navbar from "@/components/NavbarComp.vue";
+import Footer from "@/components/FooterComp.vue";
+</script>
+<template>
+  <Navbar />
+  <div class="pageContentWrapper">
+    <RouterView />
+  </div>
+  <div class="footer bg-dark">
+    <Footer />
+  </div>
+</template>
---
+++ repository-template/src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<script></script>
+<template>
+  <div class="pagInicial">
+    <div class="container-fluid text-center">
+      <img
+        src="https://i.pinimg.com/originals/80/21/74/8021744fc275101ed9a4b75ace41f168.gif"
+        alt=" "
+      />
+    </div>
+  </div>
+</template>
+<style scoped>
+.container-fluid {
+  margin-top: 20px;
+  max-width: auto;
+  max-height: auto;
+}
+</style>
